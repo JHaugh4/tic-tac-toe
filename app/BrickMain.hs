88a8b8533010097c4fbe97b3e6188b1f57e25f11
Joseph Haugh <jhaugh500@gmail.com>
2024-04-16 10:16:42 -0600

    added the ability to move around the board with the arrow keys.

--- app/BrickMain.hs
+++ app/BrickMain.hs
@@ -2,10 +2,10 @@ module BrickMain where
 
 import Brick.Main (App(..), defaultMain, neverShowCursor, resizeOrQuit, findClickedExtents)
 import Brick.Types (Widget, BrickEvent(..), EventM, get, put)
-import Brick.Widgets.Core (str, withAttr, clickable, (<+>), vBox)
+import Brick.Widgets.Core (str, withAttr, clickable, (<+>), vBox, hBox)
 import Brick.Widgets.Center (center)
 import Brick.Widgets.Table (table, renderTable)
-import Brick.Widgets.Border (border)
+import Brick.Widgets.Border (border, hBorder, vBorder)
 import Brick.AttrMap (AttrMap, attrMap, AttrName, attrName)
 import Brick.Util (fg)
 
@@ -13,15 +13,21 @@ import qualified Graphics.Vty as V
 import Graphics.Vty.Input.Events (Button(..))
 
 import Data.Char (isDigit)
+import Data.List (intersperse, intercalate)
 
 import TicTacToe (Grid, Player(..), size, emptyGrid, next, isWinner, fullBoard, move)
 
 import qualified Debug.Trace as DT
 
+data InputMode = Typing | Arrowing
+    deriving (Eq, Show)
+
 data St = St
     { _grid :: Grid
     , _curPlayer :: Player
-    , _cellToPlay :: String
+    , _inputMode :: InputMode
+    , _textCell :: String
+    , _arrowCell :: Int
     , _playable :: Bool
     , _statusMessage :: String
     } deriving (Eq, Show)
@@ -35,21 +41,25 @@ bAttr = attrName "b"
 xAttr :: AttrName
 xAttr = attrName "x"
 
+outputAttr :: AttrName
+outputAttr = attrName "output"
+
+selectedAttr :: AttrName
+selectedAttr = attrName "selected"
+
 getAttr :: Player -> AttrName
 getAttr p = case p of
     O -> oAttr
     B -> bAttr
     X -> xAttr
 
-outputAttr :: AttrName
-outputAttr = attrName "output"
-
 theMap :: AttrMap
 theMap = attrMap V.defAttr
-    [ (oAttr, fg V.blue)
+    [ (oAttr, fg V.green)
     , (bAttr, fg V.black)
     , (xAttr, fg V.red)
     , (outputAttr, fg V.magenta)
+    , (selectedAttr, fg V.cyan)
     ]
 
 drawCellToPlay :: String -> Widget ()
@@ -57,21 +67,31 @@ drawCellToPlay ctp =
     withAttr outputAttr
     (str "Cell Num: " <+> str ctp)
 
-drawGridCell :: Int -> Player -> Widget ()
-drawGridCell l B =
-    withAttr bAttr $
-    str (show l)
-drawGridCell l p =
-    withAttr (getAttr p) $
-    str (show p)
-
-drawGrid :: Grid -> Widget ()
-drawGrid grid = renderTable (table labeledGrid)
+drawGridCell :: Bool -> Int -> Player -> Widget ()
+drawGridCell sel i p =
+    withAttr atr $
+    str (txt pst)
     where
-        toI c r = c + (r * size)
+        atr = if sel then selectedAttr else getAttr p
+        txt = if sel then ("→" ++) else pad 2 ' '
+        pst = if p == B then show i else show p
+
+pad :: Int -> a -> [a] -> [a]
+pad i a as
+    | lenAs < i = replicate (i - lenAs) a ++ as
+    | otherwise = as
+    where
+        lenAs = length as
+
+drawGrid :: Int -> Grid -> Widget ()
+drawGrid ac grid = renderTable (table labeledGrid)
+    where
+        go c r p =
+            let i = c + (r * size)
+            in drawGridCell (i == ac) i p
         labeledGrid =
-            [ [ drawGridCell (toI c r) p | (p, c)   <- zip row  [0..] ] |
-                                           (row, r) <- zip grid [0..] ]
+            [ [ go c r p | (p, c)   <- zip row  [0..] ] |
+                           (row, r) <- zip grid [0..] ]
 
 drawCurPlayerBox :: Player -> Widget ()
 drawCurPlayerBox p =
@@ -86,9 +106,9 @@ drawStatusBox s =
 drawUI :: St -> [Widget ()]
 drawUI s = [
     vBox [
-        drawCellToPlay (_cellToPlay s),
+        drawCellToPlay (_textCell s),
         drawCurPlayerBox (_curPlayer s),
-        drawGrid (_grid s),
+        drawGrid (_arrowCell s) (_grid s),
         drawStatusBox (_statusMessage s)]]
 
 play :: Int -> St -> St
@@ -103,26 +123,50 @@ play i s
 
 checkBoard :: St -> St
 checkBoard s
-    | isWinner O (_grid s) = s { _statusMessage = "Player O wins!", _playable = False }
-    | isWinner X (_grid s) = s { _statusMessage = "Player X wins!", _playable = False }
-    | fullBoard (_grid s)  = s { _statusMessage = "It's a draw!",   _playable = False }
+    | isWinner O (_grid s) = s { _statusMessage = "Player O wins!"
+                               , _playable = False
+                               }
+    | isWinner X (_grid s) = s { _statusMessage = "Player X wins!"
+                               , _playable = False
+                               }
+    | fullBoard (_grid s)  = s { _statusMessage = "It's a draw!"
+                               , _playable = False
+                               }
     | otherwise            = s
+
+arrowKeyToOffset V.KUp    = -size
+arrowKeyToOffset V.KRight = 1
+arrowKeyToOffset V.KDown  = size
+arrowKeyToOffset V.KLeft  = -1
+arrowKeyToOffset _        = 0
 
 handleEvent :: BrickEvent () () -> EventM () St ()
 handleEvent (VtyEvent (V.EvKey V.KEnter _)) = do
     s <- get
-    let s'  = play (read (_cellToPlay s)) s
+    let i = case _inputMode s of
+                Typing   -> read (_textCell s)
+                Arrowing -> _arrowCell s
+    let s'  = play i s
     let s'' = checkBoard s'
-    put (s'' { _cellToPlay = "" })
+    put (s'' { _textCell = "" })
+    
+
 handleEvent bevent@(VtyEvent (V.EvKey (V.KChar 'q') _)) =
     resizeOrQuit bevent
 handleEvent (VtyEvent (V.EvKey (V.KChar c) _))
     | isDigit c = do
         s <- get
         if _playable s
-        then put (s { _cellToPlay = _cellToPlay s ++ [c] })
+        then put (s { _textCell = _textCell s ++ [c], _inputMode = Typing })
         else put s
-handleEvent bevent = DT.trace (show bevent) $ return ()
+handleEvent (VtyEvent (V.EvKey key mods))
+    | key `elem` [ V.KUp, V.KRight, V.KDown, V.KLeft ] = do
+        s <- get
+        let offset = arrowKeyToOffset key
+        let newAC  = (_arrowCell s + offset) `mod` (size * size)
+        put $ s { _arrowCell = newAC, _inputMode = Arrowing }
+    | otherwise = return ()
+handleEvent _ = return ()
 
 app :: App St () ()
 app = App
@@ -135,6 +179,6 @@ app = App
 
 run :: IO ()
 run = do
-    let s = St emptyGrid O "" True ""
+    let s = St emptyGrid O Typing "" 0 True ""
     _ <- defaultMain app s
     return ()
